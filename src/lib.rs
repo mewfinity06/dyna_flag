356603c73aef049ad2a3cd1af96f42b3eb78e199
Ashton Sickler <ashton.sickler@icloud.com>
2024-11-20 13:04:57 -0600

    Added err_note and note

--- src/lib.rs
+++ src/lib.rs
@@ -15,23 +15,42 @@ macro_rules! match_value {
     };
 }
 
-pub mod dyna {
+#[allow(unused_macros)]
+macro_rules! debug_match {
+    ($f:expr, $switch:expr, $(rule = $value:tt, $string:tt;)+) => {
+        #[allow(unreachable_patterns)]
+        match $switch {
+            $(
+                $value => {
+                    write!($f, "{}", $string)
+                },
+            )+
+            _ => {
+                write!($f, "Unknown value")
+            }
+        }
+    };
+}
+
+pub mod flag {
     use super::*;
 
     #[allow(dead_code)]
     pub enum FlagError {
         NoValue,
+        InvalidFlag,
+        MissingArgument,
     }
 
     #[allow(unreachable_patterns)]
-    impl Debug for FlagError {
+    impl std::fmt::Debug for FlagError {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            match self {
-                Self::NoValue => {
-                    write!(f, "Found no value in Flag")
-                }
-                _ => Ok(()),
-            }
+            use FlagError::*;
+            debug_match!(f, *self, // Dereference self to match enum variants
+                rule = NoValue, "No value provided";
+                rule = InvalidFlag, "Invalid flag";
+                rule = MissingArgument, "Missing Argument";
+            )
         }
     }
 
@@ -56,6 +75,7 @@ pub mod dyna {
             if let Some(value) = self.value.as_ref() {
                 match_value!( value, f,
                     rule = &'static str;
+                    rule = &'static bool;
                     rule = &'static i32;
                     rule = &'static f32;
                 )
@@ -108,13 +128,92 @@ pub mod dyna {
     }
 }
 
+mod note {
+    use super::*;
+    pub struct Note<'a> {
+        note: &'a str,
+    }
+
+    impl<'a> Debug for Note<'a> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result<> {
+            write!(f, "[NOTE]: {}", self.note)
+        }
+    }
+
+    #[allow(dead_code)]
+    impl<'a> Note<'a> {
+        pub fn new(note: &'a str) -> Self {
+            Self{ note }
+        }
+    }
+}
+
+mod err_note {
+    use super::*;
+    use std::process;
+
+    #[allow(dead_code)]
+    pub enum ErrType {
+        Exit,
+        Assertion,
+    }
+
+    impl Debug for ErrType {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            use ErrType::*;
+
+            debug_match!(f, *self,
+                rule = Exit, "EXIT";
+                rule = Assertion, "ASSERTION";
+            )
+        }
+    }
+
+    pub struct ErrNote<'a> {
+        err_note: &'a str,
+        type_: ErrType,
+        exit: bool,
+    }
+
+    impl<'a> Debug for ErrNote<'a> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result<> {
+            write!(f, "[{:?} ERROR]: {}", self.type_, self.err_note)
+        }
+    }
+
+    impl<'a> Drop for ErrNote<'a> {
+        fn drop(&mut self) {
+            println!("{:?}", self);
+        }
+    }
+
+    #[allow(dead_code)]
+    impl<'a> ErrNote<'a> {
+        pub fn new(err_note: &'a str, type_: ErrType, exit: bool) -> Self {
+            Self { err_note, type_, exit }
+        }
+
+        pub fn exit(&self) {
+            if self.exit {
+                println!("{:?}", self);
+                process::exit(1);
+            }
+        }
+
+        pub fn panic(&self) {
+            panic!("{:?}", self)
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*; // Import the library components
+    use super::*;
+    use super::flag::*; // Import the library components
 
     #[test]
     fn test_flag_creation() {
-        let flag = dyna::Flag {
+        let flag = Flag {
             name: "test",
             args: &["-t", "--test"],
             desc: "A test flag",
@@ -141,7 +240,7 @@ mod tests {
 
     #[test]
     fn test_flag_without_value() {
-        let flag = dyna::Flag {
+        let flag = Flag {
             name: "help",
             args: &["-h", "--help"],
             desc: "Display help information",
@@ -158,7 +257,7 @@ mod tests {
 
     #[test]
     fn test_flag_debug_format() {
-        let flag = dyna::Flag {
+        let flag = Flag {
             name: "output",
             args: &["-o", "--output"],
             desc: "Specify the output file",
@@ -179,7 +278,7 @@ mod tests {
 
     #[test]
     fn test_flag_with_bool_value() {
-        let flag = dyna::Flag {
+        let flag = Flag {
             name: "verbose",
             args: &["-v", "--verbose"],
             desc: "Enable verbose output",
